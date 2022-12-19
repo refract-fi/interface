a7b8052ecf2128287b5abfcb2546ad2e3348d62d
zeJabun <jabun@mintor.xyz>
2022-12-18 20:29:12 -0500

    Add createJob

---
+++ src/pages/api/job.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  return new Promise(async (resolve, reject) => {
+    if (_req.method === 'POST') {
+      let response = await axios.post(`https://api.refract.fi/job/create`, _req.body);
+      res.status(200).json(response.data);
+    } else if (_req.method === 'GET') {
+      let response = await axios.get('https://api.refract.fi/job/check/' + _req.query.id);
+      res.status(200).json(response.data);
+    }
+  });
+}
